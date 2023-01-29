3ed129a4c2be7e7a2a9842bba1db20ce727ffd0a
Teyik0 <teyik0.fr@icloud.com>
2023-01-29 14:22:55 +0100

    font ajouté

--- components/Home/Discover.tsx
+++ components/Home/Discover.tsx
@@ -6,16 +6,23 @@ const Discover = () => {
   return (
     <section className='flex flex-col lg:flex-row jutify-center items-center lg:justify-evenly pt-24 pb-24'>
       <div className='flex flex-col justify-center items-center'>
-        <h2 className='text-[#C6AB71] text-6xl font-caveat'>
-          Découvrir
-          <br />
-          l'histoire
+        <h2 className='text-[#C6AB71] text-4xl xl:text-5xl font-caveat text-center'>
+          Un restaurant Indien authentique
+          <br />à Savigny Le Temple
         </h2>
-        <p className='text-white text-lg w-full pr-2 pl-2 sm:p-0 sm:w-[500px] mt-4 text-center'>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quam
-          veritatis accusantium dolore laudantium quibusdam placeat officiis eum
-          deleniti eius rem doloremque quaerat animi molestias, aliquid
-          consequuntur nemo atque esse delectus ratione, mollitia maxime
+        <p className='text-white text-xl font-bold w-full pr-2 pl-2 sm:p-0 sm:w-[600px] mt-4 text-center'>
+          Namaste, Vanakam! <br />
+          Bienvenue chez INDIAN COFFEE, <br />
+        </p>
+        <p className='text-white text-base w-full pr-2 pl-2 sm:p-0 sm:w-[500px] mt-4 text-center'>
+          Nous vous proposons des plats traditionnels du Sud de l'Inde, riche en
+          couleurs et saveurs, afin de vous faire voyager dans le monde gourmet
+          de la cuisine indienne. <br /> <br />
+          Au MENU (renvoyer à la page Menu), des entrées tel que le Cheese Naan
+          fait maison, des plats à base de viande tel que le savoureux Butter
+          Chicken, des plats végétariens, des desserts, boissons et pleins
+          d'autres belles surprises pour le plaisir de vos papille et le bonheur
+          de vos familles.
         </p>
         <Link href='/contact'>
           <p
@@ -27,11 +34,11 @@ const Discover = () => {
         </Link>
       </div>
       <Image
-        src='/Le-butter-chicken.jpg'
+        src='/histoire.png'
         alt='butter-chicken'
         width={600}
         height={50}
-        className='sm:rounded-xl shadow-2xl mt-16 lg:mt-0'
+        className='sm:rounded-xl mt-16 lg:mt-0'
       />
     </section>
   );
